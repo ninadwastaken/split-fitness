58f90d95d220247f96772af47426d7edf53abfb4
ninadwastaken <ninad2eyes@gmail.com>
2024-06-18 00:47:27 +0530

    added workout data structure template to TemplateWorkoutScreen.js

--- screens/TemplateWorkoutScreen.js
+++ screens/TemplateWorkoutScreen.js
@@ -2,11 +2,51 @@ import {View, StyleSheet, Text, SafeAreaView, TouchableOpacity} from 'react-nati
 
 export default function TemplateWorkoutScreen({ route, navigation }) {
     const { workout_name } = route.params;
-    const exercises = ['incline bench', 'decline bench'];
+    const workout = {
+        workout_name: 'best workout',
+        decline_bench: {
+            1: { reps: 8, weight: 130 },
+            2: { reps: 8, weight: 150 },
+        },
+        incline_bench: {
+            1: { reps: 12, weight: 200 },
+            2: { reps: 1, weight: 90 },
+        },
+    }
+
+    // TODO! make workout page acc to sample workout data
 
     return (
         <SafeAreaView style={styles.container}>
+            {/* title */}
             <Text> {workout_name} </Text>
+
+            {/* workout list */}
+
+            {/* sample workout data:
+                workout_name:
+                    e1:
+                        1:
+                            reps: int
+                            weight: int
+                        2:
+                            reps: int
+                            weight: int
+                        ...
+                    e2:
+                        1:
+                            reps: int
+                            weight: int
+                        2:
+                            reps: int
+                            weight: int
+                        ...
+                    .
+                    .
+                    .
+            */}
+
+            {/* timer and buttons */}
             <TouchableOpacity
                 onPress={() => {navigation.navigate('TabNavigation')}}
                 style={[styles.endWorkoutButton, {bottom: 90}]}
